08472d9be979356a239f9935d7afd3ef8beca65b
Reshawn Lofters <reshawnalofters@gmail.com>
2026-03-08 20:47:59 -0400

    Implement BankingSystemBackend class

--- backend/src/banking_system_backend.py
+++ backend/src/banking_system_backend.py
@@ -1,3 +1,25 @@
+"""
+Banking System Backend
+
+The backend of the program first loads accounts from the 'master bank accounts' file.
+Afterwards, it reads transaction records from the 'merged bank account transaction' file.
+It then processes and executes transactions based on the transaction records.
+Finally, it writes an updated 'master bank accounts' and 'current bank accounts' file to output.
+
+Input files:
+- master_bank_accounts.txt: Contains all bank accounts.
+- merged_bank_account_transactions.txt: Contains all daily bank account transactions.
+
+Output files:
+- new_master_bank_accounts.txt: Contains updated bank accounts.
+- current_bank_accounts.txt: Contains all active bank accounts.
+
+Instructions:
+1. Open the terminal
+2. Change the directory to 'backend/src': cd backend/src
+3. Run this file: python banking_system_backend.py
+"""
+
 from bank_accounts import BankAccounts
 from transaction_file_reader import TransactionFileReader
 from transaction_processor import TransactionProcessor
@@ -5,6 +27,10 @@ from account_file_writer import AccountFileWriter
 
 
 class BankingSystemBackend:
+    """
+    Main class for the banking system back-end.
+    Coordinates loading bank accounts, processing transactions, and writing output files.
+    """
 
     def __init__(
         self,
@@ -13,20 +39,78 @@ class BankingSystemBackend:
         new_master_bank_accounts_file,
         current_bank_accounts_file,
     ):
-        pass
+        """
+        Constructs BankingSystemBackend objects.
+        :param master_bank_accounts_file: 'Master bank accounts' file path
+        :param merged_bank_account_transactions_file: 'Merged bank accounts' file path
+        :param new_master_bank_accounts_file: 'New master bank accounts' file path
+        :param current_bank_accounts_file: 'Current bank accounts' file path
+        """
+        self.master_bank_accounts_file = master_bank_accounts_file
+        self.merged_bank_account_transactions_file = (
+            merged_bank_account_transactions_file
+        )
+        self.new_master_bank_accounts_file = new_master_bank_accounts_file
+        self.current_bank_accounts_file = current_bank_accounts_file
+
+        self.accounts = BankAccounts()
 
     def run(self):
-        pass
+        """
+        Runs backend jobs.
+        """
+        self.load_accounts()
+        self.process_transactions()
+        self.write_new_account_files()
 
     def load_accounts(self):
-        pass
+        """
+        Loads bank accounts.
+        """
+        self.accounts.load_accounts(self.master_bank_accounts_file)
 
     def process_transactions(self):
-        pass
+        """
+        Processes transactions.
+        """
+        reader = TransactionFileReader(self.merged_bank_account_transactions_file)
+        processor = TransactionProcessor(self.accounts)
+
+        transaction_records = reader.read_transaction_records()
+        for transaction_record in transaction_records:
+            if transaction_record.startswith("00"):
+                continue
+
+            processor.process_transaction(transaction_record)
 
     def write_new_account_files(self):
-        pass
+        """
+        Writes output files.
+        """
+        writer = AccountFileWriter()
+
+        accounts = self.accounts.get_all_accounts()
+        writer.write_new_master_bank_accounts_file(
+            accounts, self.new_master_bank_accounts_file
+        )
+        writer.write_current_bank_accounts_file(
+            accounts, self.current_bank_accounts_file
+        )
 
 
 if __name__ == "__main__":
-    pass
+
+    master_bank_accounts_file = "../inputs/master_bank_accounts.txt"
+    merged_bank_account_transactions_file = (
+        "../inputs/merged_bank_account_transactions.txt"
+    )
+    new_master_bank_accounts_file = "../outputs/new_master_bank_accounts.txt"
+    current_bank_accounts_file = "../outputs/current_bank_accounts.txt"
+
+    backend = BankingSystemBackend(
+        master_bank_accounts_file,
+        merged_bank_account_transactions_file,
+        new_master_bank_accounts_file,
+        current_bank_accounts_file,
+    )
+    backend.run()
