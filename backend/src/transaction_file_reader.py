5718a185090610471f06b1c69ee9eb13b2f0abd5
naufil_ansari_05 <145029992+naufilansari05@users.noreply.github.com>
2026-03-15 17:17:37 -0400

    TransactionFileReader functionality added
    Wrote the transaction_file_reader.py according to the specifications in the design document.

--- backend/src/transaction_file_reader.py
+++ backend/src/transaction_file_reader.py
@@ -1,7 +1,28 @@
+import sys
 class TransactionFileReader:
 
     def __init__(self, merged_bank_account_transactions_file):
-        pass
+        self.merged_bank_account_transactions_file = merged_bank_account_transactions_file
 
     def read_transaction_records(self):
-        pass
+        
+        transaction_records = []
+
+        try:
+
+            with open(self.merged_bank_account_transactions_file, 'r') as file:
+
+                for line in file:
+
+                    record = line.strip('\n') # get transaction record
+                    if len(record) != 40: # If not of appropriate length
+                        print(f"ERROR: Bad input length in file '{self.merged_bank_account_transactions_file}'")
+                        sys.exit(1)
+                    transaction_records.append(record)
+
+        except FileNotFoundError: # if not found
+
+            print(f"ERROR: Transaction file '{self.merged_bank_account_transactions_file}' was not found")
+            sys.exit(1)
+
+        return transaction_records
