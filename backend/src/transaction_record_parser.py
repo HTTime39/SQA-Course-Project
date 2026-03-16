72691ed5f667ed33b8a5ddfa19b8b2ff31c9f8df
naufil_ansari_05 <145029992+naufilansari05@users.noreply.github.com>
2026-03-16 14:52:59 -0400

    Added TransactionRecordParser Functionality
    Filled in the transaction_record_parser.py according to the design document specifications.

--- backend/src/transaction_record_parser.py
+++ backend/src/transaction_record_parser.py
@@ -1,16 +1,19 @@
 class TransactionRecordParser:
 
     def parse_transaction_code(self, transaction_record):
-        pass
+        return transaction_record[0:2].strip() # first two chars are transaction code
 
     def parse_account_holder_name(self, transaction_record):
-        pass
+        return transaction_record[3:23].strip() # next 20 chars (after _) are name
 
     def parse_account_number(self, transaction_record):
-        pass
+        return transaction_record[24:29].strip() # next 5 chars (after _) are acc number
 
     def parse_amount(self, transaction_record):
-        pass
+        
+        amount_str = transaction_record[30:38].strip() # next 8 chars (after _) are amount
+        try: return float(amount_str) # if valid float
+        except ValueError: return 0.0 # otherwise
 
     def parse_misc_data(self, transaction_record):
-        pass
+        return transaction_record[39:].strip() # last chars are misc
