15ee4bbc68cadb4035b39491fafc4d6f19c0e79c
HTTime39 <n.s.dizon2005@outlook.com>
2026-03-15 20:12:56 -0400

    Implemented account_file_writer.py

--- backend/src/account_file_writer.py
+++ backend/src/account_file_writer.py
@@ -2,14 +2,22 @@ from account_record_builder import AccountRecordBuilder
 
 
 class AccountFileWriter:
-
+    """
+    Handles writing accounts to the output file.
+    """
     def __init__(self):
         pass
 
-    def write_new_master_bank_accounts_file(
-        self, accounts, new_master_bank_accounts_file
-    ):
-        pass
+    def write_new_master_bank_accounts_file(self, accounts, new_master_bank_accounts_file):
+        """
+        Writes the collection of updated account records to the ‘new master bank accounts’ file using formatted account records.
+        """
+        with open(new_master_bank_accounts_file, "w") as file:
+            file.write(accounts + "\n")
 
     def write_current_bank_accounts_file(self, accounts, current_bank_accounts_file):
-        pass
+        """
+        Writes the collection of active account records to the ‘current bank accounts’ file to be used by future front-end sessions.
+        """
+        with open(current_bank_accounts_file, "a") as file:
+            file.write(accounts + "\n")
